21d326ead2bb20f309aaafca583b2531be0455d8
Claudio Manniti <claudio.manniti@outlook.com>
2020-09-06 18:06:58 +0200

    Add new director

---
+++ Creational/DesignPatterns.Creational.Builder/Director/OsBuildingSpecifications.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.Creational.Builder.Director
+{
+    public class OsBuildingSpecifications : IBuildingSpecifications
+    {
+        public string Name { get; set; }
+
+        public string Version { get; set; }
+
+        public List<string> SystemApps { get; set; }
+    }
+}
---
+++ Creational/DesignPatterns.Creational.Builder/Director/OsManufacturer.cs
@@ -0,0 +1,24 @@
+﻿using DesignPatterns.Creational.Builder.Builder.Os;
+using DesignPatterns.Creational.Builder.Domain;
+
+namespace DesignPatterns.Creational.Builder.Director
+{
+    public class OsManufacturer<TBuilder, TElement> : IDirector<TBuilder, TElement, OsBuildingSpecifications>
+        where
+            TElement : Os
+        where 
+            TBuilder : OsBuilder<TElement>
+    {
+        public TBuilder Builder { get; set; }
+
+        public void Construct(OsBuildingSpecifications specs)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public TElement Get()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
